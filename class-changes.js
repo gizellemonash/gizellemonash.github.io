da30ed7d59f2b37b8667345b304f50cca37498f6
gizellemonash <gsak0002@student.monash.edu>
2025-06-03 13:42:05 +1000

    new changes

---
+++ class-changes.js
@@ -0,0 +1,18 @@
+const classChanges = [
+  { time: 5, target: ".highlight", add: "block" },
+  { time: 7, target: ".highlight", remove: "block" },
+  { time: 9, target: ".highlight", add: "big" },
+  { time: 12, target: ".highlight", remove: "big" },  
+  { time: 125, target: ".popup", remove: "hide" },
+  { time: 130, target: ".popup", add: "hide" },
+  { time: 168, target: ".popup2", remove: "hide2" },
+  { time: 173, target: ".popup2", add: "hide2" },
+
+  { time: 9, target: ".changeme2", add: "show" },
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 16, target: ".changeme2", add: "hidden" },
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
