3dceffa0e78d6588fc9f6711c05bfe726355bd04
gizellemonash <gsak0002@student.monash.edu>
2025-06-03 21:51:51 +1000

    first draft

--- class-changes.js
+++ class-changes.js
@@ -1,23 +1,36 @@
 const classChanges = [
-  { time: 6, target: ".grow", add: "active" },
-  { time: 7, target: ".grow", remove: "active" },
+  { time: 7, target: ".grow", add: "active" },
+  { time: 8, target: ".grow", remove: "active" },
 
-  { time: 7, target: ".mainpage", add: "active" },
-  { time: 10, target: ".mainpage", remove: "active" },
+  { time: 8, target: ".filters", add: "active" },
+  { time: 10, target: ".filters", remove: "active" },
   
-  { time: 52, target: ".highlight", add: "block" },
-  { time: 54, target: ".highlight", remove: "block" },
+  { time: 52, target: ".highlight1", add: "block" },
+  { time: 54, target: ".highlight1", remove: "block" },
+  { time: 63, target: ".highlight2", add: "block" },
+  { time: 65, target: ".highlight2", remove: "block" },
+  { time: 70, target: ".highlight3", add: "block" },
+  { time: 72, target: ".highlight3", remove: "block" },
 
-  { time: 125, target: ".pop1", add: "active" },
-  { time: 130, target: ".pop1", remove: "active" },
+  { time: 96, target: ".blur", add: "active" },
+  { time: 98, target: ".blur", remove: "active" },
+
+  { time: 110, target: ".pop1", add: "active" },
+  { time: 115, target: ".pop1", remove: "active" },
+
+  { time: 121, target: ".grow2", add: "active" },
+  { time: 123, target: ".grow2", remove: "active" },
+
+  { time: 124, target: ".pop1", add: "active" },
+  { time: 129, target: ".pop1", remove: "active" },
+
+  { time: 164, target: ".blue", add: "active" },
+  { time: 168, target: ".blue", remove: "active" },
+
   { time: 168, target: ".pop2", add: "active" },
-  { time: 173, target: ".pop2", remove: "active" },
+  { time: 169, target: ".pop2", remove: "active" },
+  { time: 170, target: ".pop2", add: "active" },
+  { time: 175, target: ".pop2", remove: "active" },
 
-  { time: 9, target: ".changeme2", add: "show" },
-  { time: 16, target: ".changeme2", remove: "show" },
-  { time: 16, target: ".changeme2", add: "hidden" },
-
-  { time: 16, target: ".changeme3", add: "show" },
-  { time: 28, target: ".changeme3", remove: "show" },
-  { time: 28, target: ".changeme3", add: "hidden" }
+  { time: 210, target: ".filters", add: "active" },
 ];
